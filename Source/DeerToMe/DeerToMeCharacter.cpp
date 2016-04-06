f015e7fedaa9d972a944695905b7ff8b67ea7a2a
Rebecca Mitchekk <rebeccamitchell88@gmail.com>
2016-04-05 21:24:02 -0400

    Added in a counter for collected deer

--- Source/DeerToMe/DeerToMeCharacter.cpp
+++ Source/DeerToMe/DeerToMeCharacter.cpp
@@ -35,7 +35,7 @@ ADeerToMeCharacter::ADeerToMeCharacter()
 	// Create a camera boom (pulls in towards the player if there is a collision)
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 	CameraBoom->AttachTo(RootComponent);
-	CameraBoom->TargetArmLength = 300.0f; // The camera follows at this distance behind the character	
+	CameraBoom->TargetArmLength = 400.0f; // The camera follows at this distance behind the character	
 	CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
 
 	// Create a follow camera
