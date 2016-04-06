e7b5ee0c699e0c06ca1db4ef2d3627bd931592ba
Dave Tamayo <djbtamayo@gmail.com>
2016-04-06 13:03:40 -0400

    Created an End Trigger class and BP to check the number of deer needed vs deer collected. Added the End Trigger to the tutorial level

---
+++ Source/DeerToMe/EndTrigger.cpp
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "DeerToMe.h"
+#include "EndTrigger.h"
+
+
+// Sets default values
+AEndTrigger::AEndTrigger()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	// Create the Overlap Sphere
+	OverlapBox = CreateDefaultSubobject<UBoxComponent>(TEXT("Overlap Box"));
+	OverlapBox->InitBoxExtent(FVector(3, 3, 3));
+	OverlapBox->AttachParent = RootComponent;
+
+	// Designate the OnOverlap function as a delegate called when an actor overlaps or leaves the sphere component
+	OverlapBox->OnComponentBeginOverlap.AddDynamic(this, &AEndTrigger::OnOverlapBegin);
+
+}
+
+// Called when the game starts or when spawned
+void AEndTrigger::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void AEndTrigger::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+}
+
+void AEndTrigger::OnOverlapBegin(class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) {
+
+	if (OtherActor && OtherActor != this && OtherComp) {
+
+		ADeerToMeCharacter* DeerCharacter = Cast<ADeerToMeCharacter>(OtherActor);
+
+		if (DeerCharacter && DeerCharacter->GetDeersCollected() == deerToCollect) {
+			
+			UE_LOG(LogClass, Warning, TEXT("GOOD JORB MAN"));
+		}
+		else {
+
+			UE_LOG(LogClass, Warning, TEXT("You have collected %i deer. You need %i."), DeerCharacter->GetDeersCollected(), deerToCollect);
+		}
+	}
+}
+
---
+++ Source/DeerToMe/EndTrigger.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "DeerToMeCharacter.h"
+#include "EndTrigger.generated.h"
+
+UCLASS()
+class DEERTOME_API AEndTrigger : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AEndTrigger();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+	/** called when something enters the sphere component */
+	UFUNCTION()
+		void OnOverlapBegin(class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UPROPERTY(EditAnywhere, Category = "EndTrigger")
+		uint8 deerToCollect;
+
+private:
+
+	UPROPERTY(VisibleAnywhere, Category = "EndTrigger")
+	class UBoxComponent* OverlapBox;
+};
